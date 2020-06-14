4d373ce240081fb6f706f21664902fd783a25832
MatiKloster <MKloster@intouchhealth.com>
2020-06-14 04:08:45 -0300

    Refactor on create album and movie forms
    * delete unused id and classes from inputs

--- resources/views/album/create.blade.php
+++ resources/views/album/create.blade.php
@@ -1,43 +1,40 @@
 @extends('index')
 @section('content')
-<div class="col-md-8 offset-md-2">
-    <div class="jumbotron">
-        <h5 class=" row justify-content-center"> CREAR ALBUM</h5>
-        <form action="" method="POST">
-            <div class="form-group">
-                <label for="formGroupExampleInput">Nombre</label>
-                <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="e.g Master of Puppets">
+<div class="jumbotron">
+    <h5 class=" row justify-content-center"> CREAR ALBUM</h5>
+    <form action="{{  route('albumStore') }}" method="POST" enctype="multipart/form-data">
+        @csrf
+        <div class="form-group">
+            <label>Nombre</label>
+            <input type="text" name="name" class="form-control" placeholder="e.g Master of Puppets">
+        </div>
+        <div class="form-group">
+            <label>Banda</label>
+            <input type="text" name="artist" class="form-control" placeholder="e.g Metallica">
+        </div>
+        <div class="form-group">
+            <label for="formGroupExampleInput2">Año</label>
+            <input type="number" min=0 name="year" class="form-control" placeholder="e.g 1986">
+        </div>
+        <div class="form-group">
+            <label>Genero</label>
+            <input type="text" name="genre" class="form-control" placeholder="e.g Thrash Metal">
+        </div>
+        <div class="form-group">
+            <label>Cantidad reservable</label>
+            <input type="number" min=0 name="quantity" class="form-control" placeholder="e.g 5">
+        </div>
+        <div class="form-group">
+            <label>Precio</label>
+            <input type="number" name="price" min="0.01" step="0.01" />
+        </div>
+        <div class="form-group">
+            <div class="custom-file">
+                <label>Eliga una imagen para la tapa</label>
+                <input type="file" name="image" class="form-control-file">
             </div>
-            <div class="form-group">
-                <label for="formGroupExampleInput2">Banda</label>
-                <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="e.g Metallica">
-            </div>
-            <div class="form-group">
-                <label for="formGroupExampleInput2">Año</label>
-                <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="e.g 1986">
-            </div>
-            <div class="form-group">
-                <label for="formGroupExampleInput2">Genero</label>
-                <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="e.g Thrash Metal">
-            </div>
-            <div class="form-group">
-                <label for="formGroupExampleInput2">Cantidad reservable</label>
-                <input type="number" class="form-control" id="formGroupExampleInput2" placeholder="e.g 5">
-            </div>
-            <div class="form-group">
-                <label for="formGroupExampleInput2">Cantidad reservable</label>
-                <input type="number" min ="0" class="form-control" id="formGroupExampleInput2" placeholder="e.g 5">
-            </div>
-            <div class="form-group">
-                <label for="formGroupExampleInput2">Precio</label>
-                <input type="number" min="0.01" step="0.01" />
-            </div>
-            <div class="form-group">
-                <label for="exampleFormControlFile1">Eliga una imagen para la tapa</label>
-                <input type="file" class="form-control-file" id="exampleFormControlFile1">
-            </div>
-            <button type="submit" name="submit" class="btn btn-dark">Crear</button>
-        </form>
-    </div>
+        </div>
+        <button type="submit" name="submit" class="btn btn-dark">Crear</button>
+    </form>
 </div>
 @endsection
--- resources/views/movie/create.blade.php
+++ resources/views/movie/create.blade.php
@@ -1,39 +1,38 @@
 @extends('index')
 @section('content')
-<div class="col-md-8 offset-md-2">
-    <div class="jumbotron">
-        <h5 class=" row justify-content-center"> CREAR PELICULA</h5>
-        <form action="" method="POST">
-            <div class="form-group">
-                <label for="formGroupExampleInput">Nombre</label>
-                <input type="text" class="form-control" id="formGro upExampleInput" placeholder="e.g Titanic">
-            </div>
-            <div class="form-group">
-                <label for="formGroupExampleInput2">Director</label>
-                <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="e.g James Cameron">
-            </div>
-            <div class="form-group">
-                <label for="formGroupExampleInput2">Año</label>
-                <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="e.g 1997">
-            </div>
-            <div class="form-group">
-                <label for="formGroupExampleInput2">Genero</label>
-                <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="e.g Drama">
-            </div>
-            <div class="form-group">
-                <label for="formGroupExampleInput2">Cantidad reservable</label>
-                <input type="number" class="form-control" min="0" id="formGroupExampleInput2" placeholder="e.g 5">
-            </div>
-            <div class="form-group">
-                <label for="formGroupExampleInput2">Precio</label>
-                <input type="number" min="0.01" step="0.01" />
-            </div>
-            <div class="form-group">
-                <label for="exampleFormControlFile1">Eliga una imagen para la tapa</label>
-                <input type="file" class="form-control-file" id="exampleFormControlFile1">
-            </div>
-            <button type="submit" name="submit" class="btn btn-dark">Crear</button>
-        </form>
-    </div>
+<div class="jumbotron">
+    <h5 class=" row justify-content-center"> CREAR PELICULA</h5>
+    <form action="{{   route('movieStore')  }}" method="POST">
+        <div class="form-group">
+            <label>Nombre</label>
+            <input type="text" name="name" class="form-control" placeholder="e.g Titanic">
+        </div>
+        <div class="form-group">
+            <label>Director</label>
+            <input type="text" name="director" class="form-control" placeholder="e.g James Cameron">
+        </div>
+        <div class="form-group">
+            <label>Año</label>
+            <input type="number" min=0 name="year" class="form-control" placeholder="e.g 1997">
+        </div>
+        <div class="form-group">
+            <label>Genero</label>
+            <input type="text" name="genre" class="form-control" placeholder="e.g Drama">
+        </div>
+        <div class="form-group">
+            <label>Cantidad reservable</label>
+            <input type="number" name="quantity" class="form-control" min="0" placeholder="e.g 5">
+        </div>
+        <div class="form-group">
+            <label>Precio</label>
+            <input type="number" name="price" min="0.01" step="0.01" />
+        </div>
+        <div class="form-group">
+            <label>Eliga una imagen para la tapa</label>
+            <input type="file" name="image" class="form-control-file">
+        </div>
+        <button type="submit" name="submit" class="btn btn-dark">Crear</button>
+    </form>
 </div>
+
 @endsection
