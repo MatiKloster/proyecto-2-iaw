a11f5aaf969e7c4bee97fb2181b243ce67cd0d3f
MatiKloster <MKloster@intouchhealth.com>
2020-06-14 21:05:00 -0300

    Update Album create form to support Movie creation

--- resources/views/album/create.blade.php
+++ resources/views/album/create.blade.php
@@ -1,24 +1,47 @@
 @extends('index')
 @section('content')
 <div class="jumbotron">
+    @if($movie ?? ''=='true')
+    <h5 class=" row justify-content-center"> CREAR PELICULA</h5>
+    <form action="{{  route('movieStore') }}" method="POST" enctype="multipart/form-data">
+    @else
     <h5 class=" row justify-content-center"> CREAR ALBUM</h5>
     <form action="{{  route('albumStore') }}" method="POST" enctype="multipart/form-data">
+    @endif
+    @if (count($errors) > 0)
+        <div class="alert alert-danger">
+            <strong>Ups!</strong> Hay un par de problemas con las inputs.<br><br>
+            <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+            </ul>
+        </div>
+    @endif
+    
         @csrf
         <div class="form-group">
             <label>Nombre</label>
-            <input type="text" name="name" class="form-control" placeholder="e.g Master of Puppets">
+            <input type="text" name="name" class="form-control" placeholder="e.g Viva la vida">
         </div>
+        @if($movie ?? ''=='true')
+        <div class="form-group">
+            <label>Director</label>
+            <input type="text" name="director" class="form-control" placeholder="e.g James Cameron">
+        </div>
+        @else
         <div class="form-group">
             <label>Banda</label>
-            <input type="text" name="artist" class="form-control" placeholder="e.g Metallica">
+            <input type="text" name="artist" class="form-control" placeholder="e.g Coldplay">
         </div>
+        @endif
         <div class="form-group">
             <label for="formGroupExampleInput2">Año</label>
-            <input type="number" min=0 name="year" class="form-control" placeholder="e.g 1986">
+            <input type="number" min=0 name="year" class="form-control" placeholder="e.g 1981">
         </div>
         <div class="form-group">
             <label>Genero</label>
-            <input type="text" name="genre" class="form-control" placeholder="e.g Thrash Metal">
+            <input type="text" name="genre" class="form-control" placeholder="e.g Clasico">
         </div>
         <div class="form-group">
             <label>Cantidad reservable</label>
