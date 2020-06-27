7a4bbd3ca3289a8bf1e2adb68bd67037a8bc3779
MatiKloster <MKloster@intouchhealth.com>
2020-06-27 19:06:48 -0300

    Add Eliminar button on book list

--- resources/views/book/index.blade.php
+++ resources/views/book/index.blade.php
@@ -8,6 +8,7 @@
         <th scope="col">Usuario</th>
         <th scope="col">Nombre</th>
         <th scope="col">Reservado</th>
+        <th scope="col">Accion</th>
       </tr>
     </thead>
     <tbody>
@@ -16,6 +17,13 @@
         <td>{{$album->userName}}</td>
         <td>{{$album->albumName}}</td>
         <td>{{$album->createdAt}}</td>
+        <td>
+          <form action="{{route('albumBookedDeleteForUser',['id' => $album->id,'userId' => $album->userId])}}" method="POST">
+            @method('DELETE')
+            @csrf
+            <button type="submit" class="btn btn-lg textButton"  onclick="return confirm('Estas por cancelar la reserva, estás seguro?')">Cancelar</button>
+        </form>
+        </td>
       </tr>
     @endforeach
     @foreach ($movies as $movie)
@@ -23,6 +31,13 @@
         <td>{{$movie->userName}}</td>
         <td>{{$movie->movieName}}</td>
         <td>{{$movie->createdAt}}</td>
+        <td>
+          <form action="{{route('albumBookedDeleteForUser',['id' => $movie->id,'userId' => $movie->userId])}}" method="POST">
+            @method('DELETE')
+            @csrf
+            <button type="submit" class="btn btn-lg textButton"  onclick="return confirm('Estas por cancelar la reserva, estás seguro?')">Cancelar</button>
+        </form>
+      </td>
       </tr>
     @endforeach
     </tbody>
