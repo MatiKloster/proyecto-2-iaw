a224f532fce7eb7ad5e37281d8836c9a597ca3d6
MatiKloster <MKloster@intouchhealth.com>
2020-06-27 21:45:19 -0300

    Add API funcionality

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Movie;
+use App\Http\Resources\Movie as MovieResource;
+use App\Album;
+use App\Http\Resources\Movie as AlbumResource;
+use App\Http\Resources\Image as ImageResource;
+use App\User;
+use App\Http\Resources\AlbumCollection;
+use App\Http\Resources\MovieCollection;
+use Illuminate\Database\Eloquent\Collection;
+
+class ApiController extends Controller
+{
+    //
+    public function movies(){
+        return new MovieCollection(Movie::all());
+    }
+    public function movie($id){
+        return new MovieResource(Movie::findOrFail($id));
+    }
+    public function movieImage($id){
+        return new ImageResource(Movie::findOrFail($id));
+    }
+    
+    public function albums(){
+        return new AlbumCollection(Album::all());
+    }
+    
+    public function album($id){
+        return new AlbumResource(Album::findOrFail($id));
+    }
+
+    public function albumImage($id){
+        return new ImageResource(Album::findOrFail($id));
+    }
+
+    public function userAlbumbooks($id){
+        $user = User::findOrFail($id);
+        $albums = $user->albums()->get();
+
+        return new AlbumCollection($albums);
+    }
+    public function userMoviebooks($id){
+        $user = User::findOrFail($id);
+        $albums = $user->movies()->get();
+
+        return new MovieCollection($albums);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Resources\ProductCollection;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Movie;
+use App\Album;
+use App\Http\Resources\AlbumCollection;
+use App\Http\Resources\MovieCollection;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +22,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/movies', 'ApiController@movies');
+Route::get('/movies/{id}', 'ApiController@movie');
+Route::get('/movies/image/{id}', 'ApiController@movieImage');
+
+Route::get('/albums', 'ApiController@albums');
+Route::get('/albums/{id}', 'ApiController@album');
+Route::get('/albums/image/{id}', 'ApiController@albumImage');
+
+Route::get('/user/albums/{id}', 'ApiController@userAlbumBooks');
+Route::get('/user/movies/{id}', 'ApiController@userMovieBooks');
+
