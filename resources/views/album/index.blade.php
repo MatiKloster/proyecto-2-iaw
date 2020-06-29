37420a5b5d3b60512ed7636c99fbceb4c59779d9
MatiKloster <MKloster@intouchhealth.com>
2020-06-29 00:21:31 -0300

    Add placeholder on indexs' search

--- resources/views/album/index.blade.php
+++ resources/views/album/index.blade.php
@@ -9,7 +9,7 @@
 <div class="col ml-auto">
     <form action="{{route('albumSearch')}}" method="get" onsubmit="checkInput()">
         <div class="input-group">
-            <input type="search" name="search" class="form-control" id="inputSearch">
+            <input type="search" name="search" class="form-control" placeholder="Busqueda por nombre" id="inputSearch">
             <span class="input-group-prepend">
                 <button type="submit"><svg class="bi bi-search" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd" d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/>
