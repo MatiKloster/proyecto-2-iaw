c554dd0917c3f38e3f01f10b35011b3b404e193e
MatiKloster <MKloster@intouchhealth.com>
2020-06-28 23:13:05 -0300

    Add welcome text

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Gracias por elegirnos!</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -13,8 +13,25 @@
                             {{ session('status') }}
                         </div>
                     @endif
+                    Somos Retromundo. Una página que no quiere que el tiempo pase...<br>
+                    Pero como eso es imposible, aquí conmemoramos algunos de los muchísimos clásicos en el mundo musical y cinematográfico. <br>
+                    <br>
+                    Bajo la pestaña de Discos si adivinás lo que vas a encontrar te ganás un billete de 2 pesos <br>
+                    Ni hablar si te pregunto por la de Peliculas, no? <br>
+                    <br>
+                    Sentite libre de reservar lo que quieras, total...que se jodan los otros, vos ya lo tenés reservado. <br>
+                    Para eso, tenes que clickear el producto y vas a encontrar el botón que buscas. <br>
+                    También, vas a encontrar en la pestaña donde aparece tu nombre(o no si pusiste cualquier cosa): <br>
+                    > Una pestaña que te va a permitir ver tus reservas, vas a encontrar un botón para cancelarlas. <br>
+                    > Otra pestaña que la podes usar para cambiar tu contraseña si no te gustó cuando te la hiciste <br>
+                    > Token? Si estás interesado, podes usar el token para comunicarte con la API, detalles en Github <br>
+                    > La ultima, te permite deslogear de Retromundo! <br>
+                    <br>
+                    Espero que disfrutes usando mi App <br>
+                    Saludos!<br>
 
-                    Ya estas logeado!
+                        
+
                 </div>
             </div>
         </div>
