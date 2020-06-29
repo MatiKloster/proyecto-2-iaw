ad246b38fd7e2385d5b3d8703ef67e862c53e2c5
MatiKloster <MKloster@intouchhealth.com>
2020-06-28 23:58:34 -0300

    Fix bug on edit album

--- resources/views/album/form.blade.php
+++ resources/views/album/form.blade.php
@@ -1,6 +1,6 @@
 <div class="form-group">
     <label>Nombre</label>
-    <input type="text" name="name" class="form-control" placeholder="e.g Load" value="@if (isset($album->name)){{old('name','asdasd')}}@else {{old('name',NULL)}}@endif">
+    <input type="text" name="name" class="form-control" placeholder="e.g Load" value="@if (isset($album->name)){{old('name',$album->artist)}}@else {{old('name',NULL)}}@endif">
 </div>
 <div class="form-group">
     <label>Artista </label>
